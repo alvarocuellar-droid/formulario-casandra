aca423e35b430435cca9fd2b7835df7e49d07f6f
Alvaro <alvaro.cuellar@thetest.es>
2026-02-19 12:34:58 +0100

    avances

--- casandra-formulario.php
+++ casandra-formulario.php
@@ -10,12 +10,12 @@ Author: Alvaro Cuellar
 function casandraformulario_shortcode()
 { // Función para generar el formulario
     ob_start();
-    ?>
+?>
     <div class="contenedor-formulario">
         <form class="multi-step-form" action="javascript:" method="POST" id="cf-form">
 
             <div class="step active visible" id="fc-step-inicio">
-
+                <h2>PIDE CITA</h2>
                 <div class="cf-section cf-personal-data">
                     <input type="text" class="cf-input" id="cf-name" name="cf-name" placeholder="NOMBRE Y APELLIDOS" required><br>
                     <input type="tel" class="cf-input" id="cf-telephone" name="cf-telephone" placeholder="TELÉFONO" required><br>
@@ -53,18 +53,19 @@ function casandraformulario_shortcode()
                     </div>
                 </div>
             </div>
+
             <!-- Pantalla 2 -->
             <div class="step" id="fc-step-test-1">
-                <span id="cf-test1-title">2.Situación Actual</span>
+                <h3 id="cf-test1-title">2.Situación Actual</h3>
                 <div class="cf-step" id="step-situacion">
-                    
+
 
                     <div class="cf-section">
                         <div class="cf-question-row">
                             <label class="cf-checkbox-modern">
                                 <input type="checkbox" name="cf_embarazada" value="si">
                                 <span class="cf-checkmark"></span>
-                                <span class="cf-question-text">¿Podrías estar embarazada?</span>
+                                <span class="cf-question-text">Creo que estoy embarazada</span>
                             </label>
                         </div>
 
@@ -72,7 +73,7 @@ function casandraformulario_shortcode()
                             <label class="cf-checkbox-modern">
                                 <input type="checkbox" name="cf_pecho" value="si">
                                 <span class="cf-checkmark"></span>
-                                <span class="cf-question-text">¿Estás dando el pecho?</span>
+                                <span class="cf-question-text">Estoy dando el pecho</span>
                             </label>
                         </div>
 
@@ -80,7 +81,7 @@ function casandraformulario_shortcode()
                             <label class="cf-checkbox-modern">
                                 <input type="checkbox" name="cf_cirugia" id="cf_cirugia" value="1">
                                 <span class="cf-checkmark"></span>
-                                <span class="cf-question-text">¿Tienes cirugía ginecológica?</span>
+                                <span class="cf-question-text">Tengo cirugía ginecológica</span>
                             </label>
                             <div data-toggle-id="cf_cirugia" data-toggle-show-if-value="1">
                                 <input type="text" name="cf_cirugia_cual" id="cf_cirugia_input" class="cf-input-sub" placeholder="¿Cuál?">
@@ -92,7 +93,7 @@ function casandraformulario_shortcode()
                         <h4>Última regla:</h4>
                         <div class="cf-question-row">
                             <label class="cf-checkbox-modern">
-                                <input type="checkbox" name="cf_regla_12" id="cf_regla_trigger">
+                                <input type="radio" name="cf_regla" id="cf_regla_trigger">
                                 <span class="cf-checkmark"></span>
                                 <span class="cf-question-text">Menos de 12 meses</span>
                             </label>
@@ -100,7 +101,7 @@ function casandraformulario_shortcode()
 
                         <div class="cf-question-row">
                             <label class="cf-checkbox-modern">
-                                <input type="checkbox" name="cf_regla_mas_12">
+                                <input type="radio" name="cf_regla">
                                 <span class="cf-checkmark"></span>
                                 <span class="cf-question-text">Más de 12 meses</span>
                             </label>
@@ -108,7 +109,7 @@ function casandraformulario_shortcode()
 
                         <div class="cf-question-row">
                             <label class="cf-checkbox-modern">
-                                <input type="checkbox" name="cf_regla_nose">
+                                <input type="radio" name="cf_regla">
                                 <span class="cf-checkmark"></span>
                                 <span class="cf-question-text">No lo sé</span>
                             </label>
@@ -129,7 +130,7 @@ function casandraformulario_shortcode()
                   </div>';
                         }
                         ?>
-                    </div>
+                    </div><br>
 
                 </div>
 
@@ -146,7 +147,124 @@ function casandraformulario_shortcode()
             </div>
             <!-- Pantalla 4 -->
             <div class="step" id="fc-step-test-3">
-                <h3>Test 3</h3>
+
+                <div class="cf-step" id="step-antecedentes">
+                    <p class="cf-fake-h3">4. ANTECEDENTES PERSONALES DE IMPORTANCIA</p>
+
+                    <div class="cf-section">
+                        <p class="cf-sub-title">¿Padeces alguna enfermedad?</p>
+
+                        <div class="cf-question-group">
+                            <label class="cf-checkbox-modern">
+                                <input type="checkbox" name="cf_hipertension" id="cf_hipertension" value="1">
+                                <span class="cf-checkmark"></span>
+                                <span class="cf-question-text">Hipertensión</span>
+                            </label>
+                            <div data-toggle-id="cf_hipertension" data-toggle-show-if-value="1">
+                                <label class="cf-checkbox-modern">
+                                    <input type="checkbox" name="cf_hipertension_estable">
+                                    <span class="cf-checkmark"></span>
+                                    <span class="cf-question-text">¿Estable con medicación?</span>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="cf-question-group">
+                            <label class="cf-checkbox-modern">
+                                <input type="checkbox" name="cf_diabetes" id="cf_diabetes" value="1">
+                                <span class="cf-checkmark"></span>
+                                <span class="cf-question-text">Diabetes</span>
+                            </label>
+                            <div data-toggle-id="cf_diabetes" data-toggle-show-if-value="1">
+                                <label class="cf-checkbox-modern">
+                                    <input type="checkbox" name="cf_diabetes_estable">
+                                    <span class="cf-checkmark"></span>
+                                    <span class="cf-question-text">¿Estable con medicación?</span>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="cf-question-group">
+                            <label class="cf-checkbox-modern">
+                                <input type="checkbox" name="cf_coagulacion" id="cf_coagulacion" value="1">
+                                <span class="cf-checkmark"></span>
+                                <span class="cf-question-text">Alteración de la coagulación diagnosticada</span>
+                            </label>
+                            <div data-toggle-id="cf_coagulacion" data-toggle-show-if-value="1">
+                                <input type="text" name="cf_coagulacion_cual" id="cf_coagulacion_input" class="cf-input-sub" placeholder="¿Cuál?">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="cf-section">
+                        <p class="cf-sub-title">¿Tienes o has tenido alguna vez?</p>
+
+                        <label class="cf-checkbox-modern">
+                            <input type="checkbox" name="cf_migrana">
+                            <span class="cf-checkmark"></span>
+                            <span class="cf-question-text">Migraña con aura</span>
+                        </label>
+                        <br>
+                        <label class="cf-checkbox-modern">
+                            <input type="checkbox" name="cf_hepatica">
+                            <span class="cf-checkmark"></span>
+                            <span class="cf-question-text">Enfermedad hepática grave</span>
+                        </label>
+                        <br>
+                        <label class="cf-checkbox-modern">
+                            <input type="checkbox" name="cf_sangrado">
+                            <span class="cf-checkmark"></span>
+                            <span class="cf-question-text">Sangrado vaginal sin diagnóstico</span>
+                        </label>
+                        <br>
+                        <label class="cf-checkbox-modern">
+                            <input type="checkbox" name="cf_endometriosis">
+                            <span class="cf-checkmark"></span>
+                            <span class="cf-question-text">Endometriosis</span>
+                        </label>
+                        <br>
+                        <div class="cf-question-group">
+                            <label class="cf-checkbox-modern">
+                                <input type="checkbox" name="cf_cancer_mama" id="cf_cancer_mama" value="1">
+                                <span class="cf-checkmark"></span>
+                                <span class="cf-question-text">Cáncer de mama</span>
+                            </label>
+                            <div data-toggle-id="cf_cancer_mama" data-toggle-show-if-value="1">
+                                <label class="cf-checkbox-modern">
+                                    <input type="checkbox" name="cf_mama_receptores">
+                                    <span class="cf-checkmark"></span>
+                                    <span class="cf-question-text">¿Con receptores positivos?</span>
+                                </label>
+                            </div>
+                        </div>
+
+                        <label class="cf-checkbox-modern">
+                            <input type="checkbox" name="cf_cancer_gine">
+                            <span class="cf-checkmark"></span>
+                            <span class="cf-question-text">Cáncer ginecológico (útero, endometrio u ovario)</span>
+                        </label>
+                        <br>
+                        <label class="cf-checkbox-modern">
+                            <input type="checkbox" name="cf_cancer_activo">
+                            <span class="cf-checkmark"></span>
+                            <span class="cf-question-text">Cáncer activo actualmente</span>
+                        </label>
+                        <br>
+                        <label class="cf-checkbox-modern">
+                            <input type="checkbox" name="cf_trombosis">
+                            <span class="cf-checkmark"></span>
+                            <span class="cf-question-text">Trombosis venosa o embolia pulmonar</span>
+                        </label>
+                        <br>
+                        <label class="cf-checkbox-modern">
+                            <input type="checkbox" name="cf_ictus">
+                            <span class="cf-checkmark"></span>
+                            <span class="cf-question-text">Infarto o ictus</span>
+                        </label>
+                    </div>
+                    <br>
+                    
+                </div>
                 <button type="button" class="cf-btn-main" data-goto="fc-step-test-2">Volver</button>
                 <button type="button" class="cf-btn-main" data-goto="fc-step-disponibilidad">Ver disponibilidad</button>
 
@@ -159,7 +277,7 @@ function casandraformulario_shortcode()
         </form>
     </div>
 
-    <?php
+<?php
     return ob_get_clean();
 }
 
@@ -167,25 +285,25 @@ function casandra_cargar_estilos()
 { // Función para cargar los estilos CSS
     wp_enqueue_style('casandraformulario-style', plugin_dir_url(__FILE__) . 'assets/css/style.css');
     wp_enqueue_script(
-            'cf-pills',
-            plugin_dir_url(__FILE__) . 'assets/js/pills.js',
-            array(),
-            '1.0',
-            true
+        'cf-pills',
+        plugin_dir_url(__FILE__) . 'assets/js/pills.js',
+        array(),
+        '1.0',
+        true
     );
     wp_enqueue_script(
-            'cf-steps',
-            plugin_dir_url(__FILE__) . 'assets/js/steps.js',
-            array(), // Dependencias (ninguna por ahora)
-            '1.0',
-            true     // Cargar en el footer para que no ralentice la web
+        'cf-steps',
+        plugin_dir_url(__FILE__) . 'assets/js/steps.js',
+        array(), // Dependencias (ninguna por ahora)
+        '1.0',
+        true     // Cargar en el footer para que no ralentice la web
     );
     wp_enqueue_script(
-            'cf-toggle',
-            plugin_dir_url(__FILE__) . 'assets/js/toggle.js',
-            array(), // Dependencias (ninguna por ahora)
-            '1.0',
-            true     // Cargar en el footer para que no ralentice la web
+        'cf-toggle',
+        plugin_dir_url(__FILE__) . 'assets/js/toggle.js',
+        array(), // Dependencias (ninguna por ahora)
+        '1.0',
+        true     // Cargar en el footer para que no ralentice la web
     );
 }
 
